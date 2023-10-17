67e61e7e66fbe6c938707df6031ec73320a945a4
Юрий Петриков <146861042+YuriiPetrikov@users.noreply.github.com>
2023-10-17 22:16:17 +0700

    Update and rename 1 to Category.cs

---
+++ 02 Полиморфизм/Category.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.DataStructure
+{
+    public class Category : IComparable
+    {
+        public readonly string Name;
+        public readonly MessageType Type;
+        public readonly MessageTopic Topic;
+        public Category(string _name, MessageType _messageType, MessageTopic _messageTopic)
+        {
+            Name = _name;
+            Type = _messageType;
+            Topic = _messageTopic;
+        }
+
+        public static bool operator <= (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) <= 0;
+        }
+        public static bool operator >= (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) >= 0;
+        }
+        public static bool operator == (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) == 0;
+        }
+        public static bool operator != (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) != 0;
+        }
+        public static bool operator > (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) > 0;
+        }
+        public static bool operator < (Category сategory1, Category сategory2)
+        {
+            return сategory1.CompareTo(сategory2) < 0;
+        }
+
+        public override string ToString()
+        {
+            return Name + "." + Type + "." + Topic;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Category)) return 1;
+            
+            var other = (Category)obj;
+            
+            var nameComparison = string.Compare(Name, other.Name);
+            var typeComparison = Type.CompareTo(other.Type);
+            var topicComparison = Topic.CompareTo(other.Topic);
+            
+            if (nameComparison != 0) return nameComparison;
+            if (typeComparison != 0) return typeComparison;
+            if (topicComparison != 0) return topicComparison;
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Category) || this.GetHashCode() != obj.GetHashCode())
+                return false;
+
+            var other = (Category)obj;
+            return Name == other.Name && Type == other.Type && Topic == other.Topic;
+        }
+
+        public override int GetHashCode()
+        {
+            if (Name == null) return base.GetHashCode();
+            var hash = 13;
+            hash = (hash * 7) + Name.GetHashCode();
+            hash = (hash * 7) + Topic.GetHashCode();
+            hash = (hash * 7) + Type.GetHashCode();
+            return hash;
+        }
+    }
+}
